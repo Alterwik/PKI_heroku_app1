4014e27e1a95a1f3e7a94100662e13afd1e85469
Wiktor <wiktor.nowacki95@gmail.com>
2020-04-19 18:25:59 +0200

    port debug 2

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
+const PORT = process.env.PORT || 5000
+
 app.get('*', function (req, res) {
   res.send('Hello World!');
 });
-var port_number = server.listen(process.env.PORT || 3000);
-app.listen(port_number, function () {
-  console.log('Example app listening on port!');
+app.listen(PORT, function () {
+  console.log(`Example app listening on port: ${ PORT }!`);
 });
